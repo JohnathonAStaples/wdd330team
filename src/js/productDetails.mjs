b9ec88ba821bf689eb7be9aa394798d4540e6411
kellyannclark <thefaithseed@gmail.com>
2023-10-05 12:07:02 -0600

    product not found functionality added

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -4,12 +4,30 @@ import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 let product = {};
 
 export default async function productDetails(productId) {
-  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-  product = await findProductById(productId);
-  // once we have the product details we can render out the HTML
-  renderProductDetails();
-  // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", addToCart);
+  try {
+    // get the details for the current product. findProductById will return a promise!
+    product = await findProductById(productId);
+    
+    // Check if the product is found
+    if (product) {
+      // once we have the product details we can render out the HTML
+      renderProductDetails();
+      // once the HTML is rendered we can add a listener to Add to Cart button
+      document.getElementById("addToCart").addEventListener("click", addToCart);
+    } else {
+      // Handle the case where the product is not found
+      displayProductNotFoundError();
+      // Hide the Add to Cart button when no product is found
+      document.getElementById("addToCart").style.display = "none";
+
+    }
+  } catch (error) {
+    // Handle any other errors that might occur during the API request
+    console.error("Error fetching product details:", error);
+    displayError("An error occurred while fetching product details.");
+    // Hide the Add to Cart button in case of an error
+    document.getElementById("addToCart").style.display = "none";
+  }
 }
 function addToCart() {
   const storedCart = getLocalStorage("so-cart");
@@ -20,7 +38,6 @@ function addToCart() {
 
   setLocalStorage("so-cart", cart);
 }
-
 function renderProductDetails() {
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
@@ -34,3 +51,21 @@ function renderProductDetails() {
     product.DescriptionHtmlSimple;
   document.querySelector("#addToCart").dataset.id = product.Id;
 }
+function displayProductNotFoundError() {
+  // Display an error message indicating that the product was not found
+  //  show the error message
+  const errorMessageElement = document.getElementById("errorMessage");
+  errorMessageElement.innerText = "Product not found.";
+  errorMessageElement.style.display = "block";
+}
+
+function displayError(errorMessage) {
+  // Display a general error message
+  // show the error message
+  const errorMessageElement = document.getElementById("errorMessage");
+  errorMessageElement.innerText = errorMessage;
+  errorMessageElement.style.display = "block";
+}
+
+
+  
