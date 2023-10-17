4dfe4c90fa881bb3f2e59eb616266dafc9669e81
kellyannclark <thefaithseed@gmail.com>
2023-10-17 17:50:08 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,8 @@ export default defineConfig({
           __dirname,
           "src/product_pages/northface-talus-4.html"
         ),
+        productlist: resolve(__dirname, "src/product-list/index.html"),
+ 
       },
     },
   },
